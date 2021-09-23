eb9af897d9a6ae54419016e798f4dc8bf1674622
maayazed <mayahzebrasky@gmail.com>
2021-09-22 22:20:38 -0500

    commented out ALOT of source code, added useQuery and useMutation to search and saved books files in pages folder

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,40 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+// { useState, useEffect }
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
 // import { getMe, deleteBook } from '../utils/API';
-// import Auth from '../utils/auth';
-// import { removeBookId } from '../utils/localStorage';
+import Auth from '../utils/auth';
+import { removeBookId } from '../utils/localStorage';
+
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ME, GET_USER } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  // const [userData, setUserData] = useState({});
+  const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery(userParam ? GET_USER : GET_ME, {
+    variables: { username: userParam },
+  });
+  const [deleteBook, { error }] = useMutation(REMOVE_BOOK);
+
+  const userData = data?.me || data?.user || {};
 
   // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  // const userDataLength = Object.keys(userData).length;
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
+  // useEffect(() => {
+  //   const getUserData = async () => {
+  //     try {
+  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
+  //       if (!token) {
+  //         return false;
+  //       }
+  //       const response = await getMe(token);
+  //       if (!response.ok) {
+  //         throw new Error('something went wrong!');
+  //       }
+  //       const user = await response.json();
+  //       setUserData(user);
+  //     } catch (err) {
+  //       console.error(err);
+  //     }
+  //   };
+  //   getUserData();
+  // }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -44,16 +53,21 @@ const SavedBooks = () => {
       return false;
     }
 
+    // use mutation here
     try {
-      const response = await deleteBook(bookId, token);
+      // const response = await deleteBook(bookId, token);
 
-      if (!response.ok) {
+      // const updatedUser = await response.json();
+      // setUserData(updatedUser);
+      // upon success, remove book's id from localStorage
+      const { data } = await deleteBook({
+        variables: { bookId }
+      })
+
+      if (error) {
         throw new Error('something went wrong!');
       }
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
@@ -61,8 +75,20 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
+  // if (!userDataLength) {
+  //   return <h2>LOADING...</h2>;
+  // }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!userData?.username) {
+    return (
+      <div>
+        You need to be logged in to see this. Use the navigation links above to
+        sign up or log in!
+      </div>
+    );
   }
 
   return (
@@ -74,12 +100,12 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
+          {userData.savedBooks?.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
                 {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,20 @@
 import { gql } from '@apollo/client';
 
+export const GET_USER = gql`
+query user($username: String!) {
+  user(username: $username) {
+    _id
+    username
+    email
+    thoughts {
+      _id
+      thoughtText
+      createdAt
+    }
+  }
+}
+`;
+
 export const GET_ME = gql`
   query me {
     me {
